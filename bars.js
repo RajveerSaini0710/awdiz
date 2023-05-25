875e478818afc89460a6532b2ae30ad8419c145f
RajveerSaini0710 <132134163+RajveerSaini0710@users.noreply.github.com>
2023-05-25 14:43:33 +0530

    update

--- bars.js
+++ bars.js
@@ -1,3 +1,22 @@
-function onClickMenu(){
-    document.getElementById("change").classList.toggle("nav");
-}
+var online = "website"
+var website = "rajveer"
+var rajveer = 710;
+console.log("this is the example -", website , online ,rajveer );
+console.log(typeof(rajveer));
+console.log(typeof(online));
+
+// var rajveer = 12345;
+// console.log(rajveer);
+// console.log(typeof(rajveer));
+
+// var rajveer = true;
+// console.log(rajveer);
+// console.log(typeof(rajveer));
+
+// var rajveer = "12345";
+// console.log(rajveer);
+// console.log(typeof(rajveer));
+
+// var rajveer = "true";
+// console.log(rajveer);
+// console.log(typeof(rajveer));
